dd43c47212f3661ecd4f794e1a668c6a7a0f05c1
Htet Htet Khine <hhkhine169@gmail.com>
2023-03-14 22:35:14 +0630

    first commit

---
+++ Array/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        // two-dimensional Array
+        public static void Int2DArray()
+        {
+            int[,] numbers = new int[5, 2] {
+          {3,1} , {6,2} , {9,3} , {4,2} , {8,4}
+        };
+            Console.WriteLine(numbers[0, 0]);
+            Console.WriteLine(numbers[0, 1]);
+            Console.WriteLine(numbers[1, 0]);
+            Console.WriteLine(numbers[1, 1]);
+            Console.WriteLine(numbers[2, 0]);
+            Console.WriteLine(numbers[2, 1]);
+            Console.WriteLine(numbers[3, 0]);
+            Console.WriteLine(numbers[3, 1]);
+            Console.WriteLine(numbers[4, 0]);
+            Console.WriteLine(numbers[4, 1]);
+        }
+
+        // three-dimensional Array
+        public static void Int3DArray()
+        {
+            int[,,] numbers = new int[2, 2, 3] {
+           { { 1, 2, 3}, {4, 5, 6} },
+           { { 7, 8, 9}, {10, 11, 12} }
+           };
+
+            Console.WriteLine(numbers[0, 0, 0]);
+            Console.WriteLine(numbers[0, 0, 1]);
+            Console.WriteLine(numbers[0, 0, 2]);
+            Console.WriteLine(numbers[0, 1, 0]);
+            Console.WriteLine(numbers[0, 1, 1]);
+            Console.WriteLine(numbers[0, 1, 2]);
+            Console.WriteLine(numbers[1, 0, 0]);
+            Console.WriteLine(numbers[1, 0, 1]);
+            Console.WriteLine(numbers[0, 1, 2]);
+            Console.WriteLine(numbers[1, 1, 0]);
+            Console.WriteLine(numbers[1, 1, 1]);
+            Console.WriteLine(numbers[1, 1, 2]);
+        }
+
+        // four-dimensional Array
+        public static void Int4DArray()
+        {
+            int[,,,] numbers = new int[1, 2, 2, 2] {
+           {
+            { {1,2} , {3,4}} ,
+            { {5,6} , {7,8}}
+           }
+          };
+            Console.WriteLine(numbers[0, 0, 0, 0]);
+            Console.WriteLine(numbers[0, 0, 0, 1]);
+            Console.WriteLine(numbers[0, 0, 1, 0]);
+            Console.WriteLine(numbers[0, 0, 1, 1]);
+            Console.WriteLine(numbers[0, 1, 0, 0]);
+            Console.WriteLine(numbers[0, 1, 0, 1]);
+            Console.WriteLine(numbers[0, 1, 1, 0]);
+            Console.WriteLine(numbers[0, 1, 1, 1]);
+        }
+
+        // five-dimensional Array
+        public static void Int5DArray(int a, int b, int c, int d, int e)
+        {
+            int ind1 = 0;
+            int ind2 = 0;
+            int ind3 = 0;
+            int ind4 = 0;
+            int ind5 = 0;
+            int[,,,,] numbers = new int[1, 2, 2, 2, 3] {
+           {
+            {{ {1,2,3} , {4,5,6}} ,
+            {{7,8,9} , {10,11,12} }},
+            {{ {13,14,15} , {16,17,18}} ,
+            {{19,20,21} , {22,23,24} }}
+           }
+         };
+            if (numbers.GetLength(0) < 2)
+            {
+                ind1 = a;
+            }
+            if (numbers.GetLength(1) < 3)
+            {
+                ind2 = b;
+            }
+            if (numbers.GetLength(2) < 3)
+            {
+                ind3 = c;
+            }
+            if (numbers.GetLength(3) < 3)
+            {
+                ind4 = d;
+            }
+            if (numbers.GetLength(4) < 4)
+            {
+                ind5 = e;
+            }
+            Console.WriteLine(numbers[ind1, ind2, ind3, ind4, ind5]);
+
+
+            // Console.WriteLine(numbers[0, 0, 0, 0, 0]);
+            // Console.WriteLine(numbers[0, 0, 0, 0, 1]);
+            // Console.WriteLine(numbers[0, 0, 0, 0, 2]);
+            // Console.WriteLine(numbers[0, 0, 0, 1, 0]);
+            // Console.WriteLine(numbers[0, 0, 0, 1, 1]);
+            // Console.WriteLine(numbers[0, 0, 0, 1, 2]);
+            // Console.WriteLine(numbers[0, 0, 1, 0, 0]);
+            // Console.WriteLine(numbers[0, 0, 1, 0, 1]);
+            // Console.WriteLine(numbers[0, 0, 1, 0, 2]);
+            // Console.WriteLine(numbers[0, 0, 1, 1, 0]);
+            // Console.WriteLine(numbers[0, 0, 1, 1, 1]);
+            // Console.WriteLine(numbers[0, 0, 1, 1, 2]);
+            // Console.WriteLine(numbers[0, 1, 0, 0, 0]);
+            // Console.WriteLine(numbers[0, 1, 0, 0, 1]);
+            // Console.WriteLine(numbers[0, 1, 0, 0, 2]);
+            // Console.WriteLine(numbers[0, 1, 0, 1, 0]);
+            // Console.WriteLine(numbers[0, 1, 0, 1, 1]);
+            // Console.WriteLine(numbers[0, 1, 0, 1, 2]);
+            // Console.WriteLine(numbers[0, 1, 1, 0, 0]);
+            // Console.WriteLine(numbers[0, 1, 1, 0, 1]);
+            // Console.WriteLine(numbers[0, 1, 1, 0, 2]);
+            // Console.WriteLine(numbers[0, 1, 1, 1, 0]);
+            // Console.WriteLine(numbers[0, 1, 1, 1, 1]);
+            // Console.WriteLine(numbers[0, 1, 1, 1, 2]);
+        }
+
+        // foreach loop in Arrays
+
+        public static void ForloopArray()
+        {
+            int[,,,,] numbers = new int[1, 2, 2, 2, 3] {
+           {
+            {{ {1,2,3} , {4,5,6}} ,
+            {{7,8,9} , {10,11,12} }},
+            {{ {13,14,15} , {16,17,18}} ,
+            {{19,20,21} , {22,23,24} }}
+           }
+         };
+
+            // foreach loop in Array
+            Console.WriteLine("Foreach Loop \n============");
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            // for loop in Array
+            Console.WriteLine("For Loop \n=============");
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    for (int k = 0; k < numbers.GetLength(2); k++)
+                    {
+                        for (int m = 0; m < numbers.GetLength(3); m++)
+                        {
+                            for (int n = 0; n < numbers.GetLength(4); n++)
+                            {
+                                Console.WriteLine(numbers[i, j, k, m, n]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 }, { 7, 0, 9 } };
+            // Console.WriteLine(numbers.Length);
+            // Console.WriteLine(numbers[1, 2]);
+            Int2DArray();
+            Int3DArray();
+            Int4DArray();
+            Int5DArray(0, 0, 1, 1, 1);
+            Int5DArray(0, 1, 1, 1, 2);
+            ForloopArray();
+        }
+    }
+}
---
+++ starpattern/Program.cs
@@ -0,0 +1,250 @@
+﻿using System;
+namespace starpattern
+{
+    class Program
+    {
+        public static void Starpattern01()
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Starpattern02()
+        {
+            int i, j, k;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Starpattern03()
+        {
+            int i, j;
+            for (i = 5; i >= 1; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Starpattern04()
+        {
+            int i, j, k;
+            int space = 0;
+
+            for (i = 5; i > 0; i--)
+            {
+
+                for (k = 0; k < space; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+                space++;
+            }
+        }
+
+        public static void Starpattern05()
+        {
+            int i, j, k;
+            int space = 0;
+
+            for (i = 5; i > 0; i--)
+            {
+
+                for (k = 0; k < space; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                for (j = i; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+                space++;
+            }
+        }
+
+        public static void Starpattern06()
+        {
+            int i, j, space = 1;
+            space = 5;
+            for (j = 1; j <= 5; j++)
+            {
+                for (i = 1; i <= space; i++)
+                    Console.Write(" ");
+                space--;
+
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public static void Starpattern07()
+        {
+            int count = 1;
+
+            do
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                count++;
+            } while (count < 6);
+
+        }
+
+        public static void Starpattern08()
+        {
+            int i = 1, j, k;
+            do
+            {
+                for (j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                i++;
+            } while (i < 6);
+        }
+
+        public static void Starpattern09()
+        {
+            int i = 5, j;
+            do
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                i--;
+            } while (i >= 1);
+        }
+
+        public static void Starpattern10()
+        {
+            int i = 5, j, k;
+            int space = 0;
+
+            do
+            {
+
+                for (k = 0; k < space; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+                space++;
+                i--;
+            } while (i > 0);
+        }
+
+        public static void Starpattern11()
+        {
+            int i = 5, j, k;
+            int space = 0;
+
+            do
+            {
+
+                for (k = 0; k < space; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                for (j = i; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+                space++;
+                i--;
+            } while (i > 0);
+        }
+
+        public static void Starpattern12()
+        {
+            int i, j = 1, space = 1;
+            space = 5;
+            do
+            {
+                for (i = 1; i <= space; i++)
+                    Console.Write(" ");
+                space--;
+
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+                j++;
+            } while (j <= 5);
+            Console.ReadLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            Starpattern01();
+            Starpattern02();
+            Starpattern03();
+            Starpattern04();
+            Starpattern05();
+            Starpattern06();
+            Starpattern07();
+            Starpattern08();
+            Starpattern09();
+            Starpattern10();
+            Starpattern11();
+            Starpattern12();
+        }
+    }
+}
